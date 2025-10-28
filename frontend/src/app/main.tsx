7b5d91d5f02605c55a3920e8a5585eb3da6b5ca3
Максим Стрельников <maksim_stv@mail.ru>
2025-10-29 00:52:56 +0300

    первые наработки по хедеру

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import "./reset.scss";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
--- frontend/src/pages/header/Header.tsx
+++ frontend/src/pages/header/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { HouseLogo, Login, Map } from '../../shared/icons';
 import './Header.scss';
 
 interface HeaderProps {
@@ -6,8 +8,36 @@ interface HeaderProps {
 
 export const Header = ({ className }: HeaderProps) => {
 	return (
-		<div>
-
+		<div className='header'>
+			<div className="container">
+				<div className="header-up">
+					<div className="location">
+						<Map color='#999999' />
+						<p>Санкт-Петербург</p>
+					</div>
+					<div className="auth">
+						<Login color='#999999' />
+						<p>Войти</p>
+					</div>
+				</div>
+				<div className="header-main">
+					<ul className="nav__list">
+						<li className="nav__list-item">
+							<div className="logo">
+								<Link className="nav__list-link" to='/'>
+									<HouseLogo height={'60px'} width={'60px'} viewBox='0 0 25 25' />
+									<p>ООО "Бнал"</p>
+								</Link>
+							</div>
+						</li>
+						<li className="nav__list-item"><Link className="nav__list-link" to='/'>Покупка</Link></li>
+						<li className="nav__list-item"><Link className="nav__list-link" to='/news'>Аренда</Link></li>
+						<li className="nav__list-item"><Link className="nav__list-link" to='/calendar'>Новостройки</Link></li>
+						<li className="nav__list-item"><Link className="nav__list-link" to='/studboard'>Риелторы</Link></li>
+						<li className="nav__list-item"><Link className="nav__list-link" to='/contacts'>Журнал</Link></li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
