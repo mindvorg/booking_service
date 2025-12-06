b8fdb54b19270ca3918dc522dd19ea87b3ac6057
Goryunov Dima <mindvorg@gmail.com>
2025-12-06 18:25:56 +0300

    add cross

--- backend/apatment/src/main/java/com/example/controller/ApartmentsController.java
+++ backend/apatment/src/main/java/com/example/controller/ApartmentsController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/apartments")
+@CrossOrigin(origins = "*")
 public class ApartmentsController {
 
     private final ApartmentsService apartmentsService;
